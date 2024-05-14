03de59330f32c84cc8bddda23e3cd4deb76eeb0d
Francesco-Rapetti <rapettif80@yahoo.it>
2024-05-14 22:56:48 +0200

    Aggiungere i file di progetto.

---
+++ la-mia-pizzeria-static/Models/PizzaModel.cs
@@ -0,0 +1,18 @@
+﻿namespace la_mia_pizzeria_static.Models
+{
+    public class PizzaModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public double Price { get; set; }
+
+        public PizzaModel(string name, string description, double price, string image = "/img/pizza-placeholder.png")
+        {
+            this.Name = name;
+            this.Description = description;
+            this.Image = image;
+            this.Price = price;
+        }
+    }
+}
